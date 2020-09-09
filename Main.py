9791f08ad6ed29877742a07bfaff7323331dcbf9
Brandon Enns <brandonenns@Brandons-MacBook-Pro.local>
2020-09-09 14:05:19 -0700

    python basics day 1

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
-print("hello world")
+#this is how to comment using a pound
+
+''' this is a 
+multiple line comment'''
+
+# x is assigned as the integer 5 here
+x = 5 
+print(type(x))
+
+#x is changed here to a floating point number
+x = 5.5
+print(type(x))
+
+#this is a string
+my_name = "Brandon"
+print(my_name)
+
+#this is how to use exponents 2^5
+print(2**5)
+
+#to use more math functions use 
+import math
